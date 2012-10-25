d33f9620939ec38d89969b3961e4c896649fd956
Jarmo Isotalo <jamo@isotalo.fi>
2012-10-26 00:39:26 +0300

    joo toimii

--- mailer.rb
+++ mailer.rb
@@ -3,30 +3,38 @@ require 'action_mailer'
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-   :address   => "smtp.gmail.com",
-   :port      => 587,
-   :domain    => "domain.com.ar",
-   :authentication => :plain,
-   :user_name      => "test@domain.com.ar",
-   :password       => "passw0rd",
-   :enable_starttls_auto => true
+   :address   => "posti.saunalahti.fi",
+   :port      => 25,
+   :domain    => "jamo.fi",
   }
-ActionMailer::Base.view_paths= File.dirname(__FILE__)
-
+ActionMailer::Base.view_paths= "/Users/jamo/g/mailer"#File.dirname(__FILE__)
+#metodi ja mailer kansioon saman niminen html.erb filu niin kaikki ok.
+#
+#
 class Mailer < ActionMailer::Base
-
   def daily_email
     @var = "var"
 
-    mail(   :to      => "myemail@gmail.com",
-            :from    => "test@domain.com.ar",
-            :subject => "testing mail") do |format|
+    mail(   :to      => "jamo@isotalo.fi",
+            :from    => "jamo@jamo.fi",
+            :subject => "paatyykohan taa spammiin?") do |format|
                 format.text
                 format.html
     end
   end
+  
+  def my_mail
+    @name ="Jarmo"
+    @pisteet = [1,2,3,4,5]
+    mail(   :to      => "jamo@isotalo.fi",
+            :from    => "jamo@jamo.fi",
+            :subject => "paatyykohan taa spammiin?") do |format|
+                format.html
+    end    
+  end
+  
 end
 
-email = Mailer.daily_email
+email = Mailer.my_mail#daily_email
 puts email
 email.deliver

a1ad0e35011a4120e47d038a9d339b00c41dda13
Jarmo Isotalo <jamo@isotalo.fi>
2013-11-25 20:22:53 +0200

    publish

--- mailer.rb
+++ mailer.rb
@@ -1,40 +1,51 @@
+# encoding: UTF-8
+require 'pry'
 require 'action_mailer'
 
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-   :address   => "posti.saunalahti.fi",
-   :port      => 25,
-   :domain    => "jamo.fi",
+  #:address => "localhost",
+  #:port => 1025
+  :address              => "mail.cs.helsinki.fi",
+  :port                 => 587,
+  :domain               => 'cs.helsinki.fi',
+  :user_name            => 'username',
+  :password             => 'passu',
+  :authentication       => 'login',
+  :enable_starttls_auto => true
   }
 ActionMailer::Base.view_paths= "/Users/jamo/g/mailer"#File.dirname(__FILE__)
 #metodi ja mailer kansioon saman niminen html.erb filu niin kaikki ok.
-#
-#
-class Mailer < ActionMailer::Base
-  def daily_email
-    @var = "var"
 
-    mail(   :to      => "jamo@isotalo.fi",
-            :from    => "jamo@jamo.fi",
-            :subject => "paatyykohan taa spammiin?") do |format|
+class Mailer < ActionMailer::Base
+
+  def my_mail(email)
+    mail( :to      => email,
+          :from    => "User Name <username@cs.helsinki.fi>",
+          reply_to: 'username@cs.helsinki.fi',
+          :subject => "subject") do |format|
                 format.text
                 format.html
     end
   end
-  
-  def my_mail
-    @name ="Jarmo"
-    @pisteet = [1,2,3,4,5]
-    mail(   :to      => "jamo@isotalo.fi",
-            :from    => "jamo@jamo.fi",
-            :subject => "paatyykohan taa spammiin?") do |format|
-                format.html
-    end    
-  end
-  
 end
 
-email = Mailer.my_mail#daily_email
-puts email
-email.deliver
+
+mls = Proc.new {|email|
+  email_message = Mailer.my_mail email
+  puts email
+  email_message.deliver
+  puts "deliverd to #{email}"
+}
+
+end
+@emails = %W[
+ex@example.fi
+ex2@example.fi
+]
+
+@emails = @emails.uniq
+@emails.each do |e|
+  mls.call(e)
+end
